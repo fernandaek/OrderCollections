a61835d3498ccdcbdf4a119672b4b7937b3ab60d
Fernanda Ek <fernandaek@icloud.com>
2019-08-07 11:47:21 +0200

    Collection of orders

---
+++ OrderCollection/Entities/Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OrderCollection.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderCollection.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem orderItem in Items)
+            {
+                sum += orderItem.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(OrderItem item in Items)
+            {
+                stringBuilder.Append(item.ToString());
+            }
+            stringBuilder.AppendLine("Total price: $" + Total().ToString("F2"));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ OrderCollection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using OrderCollection.Entities.Enums;
+using OrderCollection.Entities;
+
+namespace OrderCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data");
+            Console.Write("Name: ");
+            var clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            var clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime dateTime = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            
+            Console.WriteLine("Enter order data");
+            Console.Write("Status: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+
+            Client client = new Client(clientName, clientEmail, dateTime);
+            Order order = new Order(DateTime.Now, orderStatus, client);
+            Console.Write("How many items to this order? ");
+            var n = int.Parse(Console.ReadLine());
+            for(var i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter #" + i + " item data");
+                Console.Write("Product name: ");
+                var nameProd = Console.ReadLine();
+                Console.Write("Product price: ");
+                var priceProd = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                var qtProd = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProd, priceProd);
+                OrderItem orderItem = new OrderItem(qtProd, priceProd, product);
+
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Order Summary");
+            Console.WriteLine("Order moment: " + DateTime.Now);
+            Console.WriteLine("Order status: " + orderStatus);
+            Console.WriteLine("Client: " + client.Name + " " + client.BirthDate + " - " + client.Email);
+
+            Console.WriteLine();
+            Console.WriteLine("Order items");
+            Console.WriteLine(order);
+            Console.WriteLine();
+
+
+        }
+    }
+}
